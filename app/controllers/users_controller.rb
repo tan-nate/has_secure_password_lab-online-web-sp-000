d6149b351cf0ea6752bc54ee39a9dd4de154a141
Nate Tan <nktan93@gmail.com>
2019-08-08 16:21:51 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def create
     User.create(user_params)
   end
- 
+
   private
- 
+
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
   end
