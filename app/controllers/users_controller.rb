26dd92b3f91aa01546090742b37b6f13ba34cf3f
Nate Tan <nktan93@gmail.com>
2019-08-08 16:41:35 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
     #binding.pry
-    user = User.create(user_params)
+    user = User.new(user_params)
     session[:user_id] = user.id
   end
 
