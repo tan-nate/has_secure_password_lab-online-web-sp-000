080cad667bf3df5738f01015807722cc6d14890b
Nate Tan <nktan93@gmail.com>
2019-08-08 16:25:33 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     #binding.pry
     return head(:forbidden) unless @user.authenticate(params[:password])
-    binding.pry
+    #binding.pry
     session[:user_id] = @user.id
   end
 end
