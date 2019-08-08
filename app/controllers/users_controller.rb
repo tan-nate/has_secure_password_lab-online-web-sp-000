51b506c67cd91b662a75254a7e1a12159d1e1c3b
Nate Tan <nktan93@gmail.com>
2019-08-08 16:43:38 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     if user.password != user.password_confirmation
       redirect_to new_user_path
     else
+      user.save
       session[:user_id] = user.id
     end
   end
